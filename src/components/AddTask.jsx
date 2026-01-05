ea7784f2b363b711bc53392ade232735a7da98f8
Naldo <lucirleylucas987@gmail.com>
2026-01-05 16:07:22 -0300

    Atualizei AddTask

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -4,7 +4,11 @@ export default function AddTask() {
             <input type="text" id="inome" className="w-full border-hidden rounded-md p-2 mb-2" placeholder="Título da tarefa"/>
             <input type="text" id="inome" className="w-full border-hidden rounded-md p-2" placeholder="Descrição da tarefa"/>
 
-            <button className="bg-slate-600 w-full rounded-md p-2 mt-2 text-white border border-transparent transition-all duration-300 ease-in-out hover:bg-white hover:text-slate-600 hover:shadow-lg hover:border-slate-700 hover:border">Adicionar</button>
+            <button 
+                className="bg-slate-600 w-full rounded-md p-2 mt-3 text-white border border-transparent transition-all duration-300 ease-in-out hover:bg-white hover:text-slate-600 hover:shadow-lg hover:border-slate-700 hover:border"
+            >
+                Adicionar
+            </button>
         </div>
     )
 }
