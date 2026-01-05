a709f4670bc7379254ade1489cfad9e5676910d6
Naldo <lucirleylucas987@gmail.com>
2026-01-05 16:02:19 -0300

    Criando app React

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import AddTask from "./components/AddTask";
 import Tasks from "./components/Tasks";
+import { ChevronRightIcon } from "lucide-react";
 
 export default function App(){
   const [tasks, setTasks] = useState([
@@ -22,11 +23,45 @@ export default function App(){
       isCompleted: false
     }
   ])
+
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map(task => {
+      if(task.id === taskId) {
+        return {
+          ...task,
+          isCompleted: !task.isCompleted
+        }
+      }
+
+      return task
+    })
+    setTasks(newTasks)
+  }
+
+  function onDeleteTaskClick(taskId){
+    const newTasks = tasks.filter(task => task.id !== taskId)
+    setTasks(newTasks)
+  }
+
+  function OnDescriptionTaskClick(taskId){
+    const newTasks = tasks.map(task => {
+      if(taskId === task.id){
+        return (
+          <dialog>{task.description}</dialog>
+        )
+      }
+      return task
+    })
+
+    setTasks(newTasks)
+  }
+
   return(
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de tarefas</h1>
-        <Tasks tasks={tasks}/>
+        <AddTask />
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick} OnDescriptionTaskClick={OnDescriptionTaskClick}/>
       </div>
     </div>
   )
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,7 +1,10 @@
 export default function AddTask() {
     return(
-        <div>
-            <h1>AddTask</h1>
+        <div className="bg-slate-300 p-6 rounded-md ">
+            <input type="text" id="inome" className="w-full border-hidden rounded-md p-2 mb-2" placeholder="Título da tarefa"/>
+            <input type="text" id="inome" className="w-full border-hidden rounded-md p-2" placeholder="Descrição da tarefa"/>
+
+            <button className="bg-slate-600 w-full rounded-md p-2 mt-2 text-white border border-transparent transition-all duration-300 ease-in-out hover:bg-white hover:text-slate-600 hover:shadow-lg hover:border-slate-700 hover:border">Adicionar</button>
         </div>
     )
 }
